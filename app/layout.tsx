5e682f736ae8b7d8e76b312922ec4f8ca7b430ec
SajaJawad <saiamaher2002@gmail.com>
2026-05-09 12:08:15 +0300

    feat: initialize root layout with global styles and custom metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,10 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Eng.Saja Jawad",
   description: "Portfolio of Eng.Saja Jawad",
+  icons: {
+    icon: "/logo.png",
+    apple: "/logo.png",
+  },
 };
 
 export default function RootLayout({
